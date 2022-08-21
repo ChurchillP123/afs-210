31bb4bb8b49d8c42d18503a4ee18cdec1c0fdf62
Churchill perry <churchill.perry@bryanuniversity.edu>
2022-08-21 16:22:24 -0500

    Shuffle Function in Python

--- week6/shuffle.py
+++ week6/shuffle.py
@@ -3,9 +3,12 @@ sampleInput = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 
 def shuffle(list):
     #the time complexity is linear O(n) since it runs through a for loop once
+    #space is fine since it uses the same array in place
     for num in list:
         rand = random.randint(0, len(list)-1)
+        #pop time complexity is O(n)
         index = list.pop(rand)
+        #append time complexity is O(1)
         list.append(index)
     return list
 
